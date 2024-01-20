ab3904ab0ac6715410510c2ccd3ecbb1542cc997
nasimulhasan <nasimulhasan@gmail.com>
2024-01-20 23:19:02 +0600

    Refactor affiliate page and update axios base URL

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default defineNuxtPlugin((NuxtApp) => {
-  axios.defaults.baseURL = process.env.API_URL;
+  // axios.defaults.baseURL = "https://api.v2.e-testpaper.com/api/v1/"
+  axios.defaults.baseURL = "http://localhost:3200/api/v1/";
 
   return {
     provide: {
